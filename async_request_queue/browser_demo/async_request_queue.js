687250a31a5471bcbea8371753ccc6284f78f71c
arcrad <jonathanmaiorana@gmail.com>
2025-01-05 00:07:44 -0500

    find bugs, fix bugs
    rework async_request_queue some more
    * fix concurrency issues
    * fix issues with starting while already processing
    * prevent timing issue where top-level promise returns before all items
      complete
    * make top-level promise return value match Promise.allSettled shape
    * add method to change max simultaneous items running
    * refactor to try to reduce redundancy

--- async_request_queue/browser_demo/async_request_queue.js
+++ async_request_queue/browser_demo/async_request_queue.js
@@ -3,7 +3,11 @@ export default class AsyncRequestQueue {
   #executionQueue = [];
   #maxSimultaneousItems = 3;
   #currentItemsRunning = 0;
+  #processCompleteHandler = null;
+  #processPromise = null;
+  #isProcessing = false;
   #id = 0;
+
   constructor(initialItems) {
     this.#queue = [...initialItems];
   }
@@ -12,47 +16,92 @@ export default class AsyncRequestQueue {
     return new Promise( async (resolve, reject) => {
       let retVal;
       try {
-        /////console.dir(item);
         retVal = await item();
         this.#currentItemsRunning--;
         await this.#process();
         resolve(retVal);
       } catch(error) {
+        this.#currentItemsRunning--;
+        await this.#process();
         reject(error); 
       } 
     });
   }
 
-  async #process() {
-    const curId = this.#id++;
-    console.log('------- process() called. curId='+curId+' ------- ');
+  #process() {
+    //const curId = this.#id++;
+    //console.log('------- process() called. curId='+curId+' ------- ');
     /////console.log('queue length = '+this.#queue.length);
-    if(this.#queue.length === 0 ) {
-      /////console.log('resolve curId='+curId);
-      /////console.groupEnd();
-      return;
-    }
-    if(this.#currentItemsRunning < this.#maxSimultaneousItems) {
+    if(this.#queue.length > 0 && this.#currentItemsRunning < this.#maxSimultaneousItems) {
       /////console.log('run another item. curId='+curId);
-      this.#executionQueue.push(this.#runItem(this.#queue.shift()));
+      this.#runItem(
+        this.#queue.shift()
+      ).then( 
+        (result) => { 
+          this.#executionQueue.push(
+            {
+              'status': 'fulfilled',
+              'value': result,
+            }
+          );
+      }).catch( 
+        (error) => { 
+          this.#executionQueue.push(
+            {
+              'status': 'rejected',
+              'reason': error,
+            }
+          );
+      }).finally(
+        () => {
+          if(this.#queue.length === 0 && this.#currentItemsRunning == 0) {
+            //allItemsDoneRunning();
+            this.#processCompleteHandler();
+            return;
+          }
+        }
+      );
       this.#currentItemsRunning++;
       /////console.dir(`currentItemsRunning=${this.#currentItemsRunning}`);
-      //await this.#process(); 
       this.#process(); 
     }
-    //return Promise.allSettled(this.#executionQueue);
-    return Promise.race(this.#executionQueue);
-
-
     /////console.log('------- end process() call with curId='+curId+' -------');
   }
 
-  async startProcessing() {
-    await this.#process();
-    return Promise.allSettled(this.#executionQueue);
+  startProcessing() {
+    if(this.#isProcessing === true) {
+      //Allow calling multiple times while processing? 
+      //return Promise.reject('Already processing');// this.#processPromise;
+      return this.#processPromise;
+    }
+    if(this.#isProcessing === false && this.#queue.length === 0) {
+      this.#processCompleteHandler();
+      return this.#processPromise;
+    }
+    this.#isProcessing = true;
+    this.#processPromise = new Promise( (resolve, reject) => {
+      this.#processCompleteHandler = () => {
+        console.log('the queue has been emptied');
+        console.dir(this.#executionQueue);
+        this.#isProcessing = false;
+        resolve(this.#executionQueue);
+      };
+      this.#process();
+    });
+    return this.#processPromise;
   }
 
   enqueue(newItem) {
     this.#queue.push(newItem);
+    if(this.#isProcessing) {
+      this.#process();
+    }
+  }
+
+  setMaxSimultaneousItems(newMax = 3) {
+    this.#maxSimultaneousItems = newMax;
+    if(this.#isProcessing) {
+      this.#process();
+    }
   }
 }
